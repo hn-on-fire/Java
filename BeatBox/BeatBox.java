196d9f138ab5db0a03205a0e9f91b4808ad97e17
hn-on-fire <hn.on.fire@gmail.com>
2021-08-14 02:26:19 +0530

    GUI
    Basic GUI With 256 check boxes complete

--- BeatBox/BeatBox.java
+++ BeatBox/BeatBox.java
@@ -1,14 +1,18 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package beatbox;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.Label;
+import java.awt.event.ActionEvent;
 import java.util.ArrayList;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.Track;
+import javax.swing.BorderFactory;
+import javax.swing.Box;
+import javax.swing.BoxLayout;
+import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -19,12 +23,71 @@ import javax.swing.JPanel;
  */
 public class BeatBox {
 
-    JPanel mainPanel;
-    ArrayList<JCheckBox> checkBoxList;
-    Sequencer sequencer;
-    Sequence sequence;
-    Track track;
-    JFrame theFrame;
-    String[] instrumentNames ={"Bass Drum", "Closed Hi-Hat", "" };
-    
+    private static JPanel mainPanel;
+    private static ArrayList<JCheckBox> checkBoxList;
+    private Sequencer sequencer;
+    private Sequence sequence;
+    private Track track;
+    private static JFrame theFrame;
+    private static final String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-Mid Tom", "High Agogo", "Open Hi Conga"};
+    private static final int instruments[] = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
+
+    public static void main(String args[]) {
+        new BeatBox().buildGui();
+    }
+
+    private void buildGui() {
+        theFrame = new JFrame("BeatBox");
+        theFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        BorderLayout layout = new BorderLayout();
+        JPanel background = new JPanel(layout);
+        background.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+        checkBoxList = new ArrayList<>();
+        Box buttonBox = new Box(BoxLayout.Y_AXIS);
+        JButton start = new JButton("Start");
+        start.addActionListener((ActionEvent a) -> {
+            buildTrackAndStart();
+        });
+        buttonBox.add(start);
+        JButton stop = new JButton("Stop");
+        stop.addActionListener((ActionEvent a) -> {
+            sequencer.stop();
+        });
+        buttonBox.add(stop);
+        JButton upTempo = new JButton("Tempo Up");
+        upTempo.addActionListener((ActionEvent a) -> {
+            sequencer.setTempoFactor((float)(sequencer.getTempoFactor()*1.03));
+        });
+        buttonBox.add(upTempo);
+        JButton downTempo = new JButton("Tempo Down");
+        downTempo.addActionListener((ActionEvent a) -> {
+            sequencer.setTempoFactor((float)(sequencer.getTempoFactor()*0.97));
+        });
+        buttonBox.add(downTempo);
+        Box nameBox = new Box(BoxLayout.Y_AXIS);
+        for(String name : instrumentNames){
+            nameBox.add(new Label(name));
+        }
+        background.add(BorderLayout.EAST, buttonBox);
+        background.add(BorderLayout.WEST, nameBox);
+        theFrame.getContentPane().add(background);
+        GridLayout grid = new GridLayout(16, 16);
+        grid.setVgap(1);
+        grid.setHgap(2);
+        mainPanel = new JPanel(grid);
+        background.add(BorderLayout.CENTER, mainPanel);
+        for (int i = 0; i < 256; i++) {
+            JCheckBox c = new JCheckBox();
+            c.setSelected(false);
+            checkBoxList.add(c);
+            mainPanel.add(c);
+        }
+        theFrame.setVisible(true);
+        theFrame.setBounds(50, 50, 300, 300);
+        theFrame.pack();
+        
+    }
+    private void buildTrackAndStart(){}
+
+   
 }
