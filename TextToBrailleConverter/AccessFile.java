d606baf237b19b918e31fccf7e5c055d413eac2a
Harsh Nandwani <hn.on.fire@gmail.com>
2021-08-23 23:28:20 +0530

    Update AccessFile.java

--- TextToBrailleConverter/AccessFile.java
+++ TextToBrailleConverter/AccessFile.java
@@ -1,20 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package brailleprinter;
-
 /**
  *
  * @author Harsh
  */
-import com.sun.speech.freetts.Voice;
-import com.sun.speech.freetts.VoiceManager;
-import java.io.BufferedInputStream;
 import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 
@@ -38,28 +26,3 @@ public class AccessFile {
             return "A System File was Deleted.Please Reinstall the Software.";
         }
     }
-
-    void read() {
-        new readFile().start();
-    }
-
-    private class readFile {
-
-        void start() {
-            System.setProperty("mbrola.base", "mbrola");
-            VoiceManager vm = VoiceManager.getInstance();
-
-            Voice v = vm.getVoice("mbrola_us2");
-
-            v.allocate();
-
-            try {
-                v.speak(new BufferedInputStream(new FileInputStream("/home/pi/Desktop/print.txt")));
-            } catch (FileNotFoundException ex) {
-                v.speak("A System File was Deleted.Please Reinstall the Software.");
-                System.err.println("A System File was Deleted.Please Reinstall the Software.");
-            }
-        }
-
-    }
-}
