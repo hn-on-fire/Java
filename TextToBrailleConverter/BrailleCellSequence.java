2dcddc5cab3d63664fd690d55ed250bdf87e261b
Harsh Nandwani <hn.on.fire@gmail.com>
2021-08-23 23:25:29 +0530

    Edited Java doc

--- TextToBrailleConverter/BrailleCellSequence.java
+++ TextToBrailleConverter/BrailleCellSequence.java
@@ -8,11 +8,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 /**
  *
  * @author Harsh
