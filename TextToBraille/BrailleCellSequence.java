0bb50bc616a3976d3190102781c9352383d63465
Harsh Nandwani <hn.on.fire@gmail.com>
2021-09-26 23:39:07 +0530

    Create BrailleCellSequence.java

---
+++ TextToBraille/BrailleCellSequence.java
@@ -0,0 +1,16 @@
+
+/**
+ *
+ * @author Harsh
+ */
+public class BrailleCellSequence {
+    /**
+     * Processes each 
+     * @param text Each line of Sentence
+     */
+    BrailleCell temp;
+    private static final String[] WHOLE_WORD_CONTRACTIONS = {"as", "be", "but", "can", "child", "do", "enough", "every", "from", "go", "have", "it", "its", "knowledge", "like", "more", "not", "out", "people", "quite", "rather", "shall", "so", "still", "that", "this", "us", "very", "was", "were", "which", "will", "you"};
+    public BrailleCellSequence(String text){
+    
+    }
+}
