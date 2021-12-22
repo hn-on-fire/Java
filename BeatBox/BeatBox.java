d08787f26e676150736ff016931e78dc67385314
hn-on-fire <hn.on.fire@gmail.com>
2021-12-22 12:48:27 +0530

    testing CLI

--- BeatBox/BeatBox.java
+++ BeatBox/BeatBox.java
@@ -43,7 +43,7 @@ public class BeatBox {
     private static JFrame theFrame;
     private static final String[] instrumentNames = {"Bass Drum", "Closed Hi-Hat", "Open Hi-Hat", "Acoustic Snare", "Crash Cymbal", "Hand Clap", "High Tom", "Hi Bongo", "Maracas", "Whistle", "Low Conga", "Cowbell", "Vibraslap", "Low-Mid Tom", "High Agogo", "Open Hi Conga"};
     private static final int instrumentKeys[] = {35, 42, 46, 38, 49, 39, 50, 60, 70, 72, 64, 56, 58, 47, 67, 63};
-
+    //test comment
     public static void main(String args[]) {
         new BeatBox().buildGui();
     }
